8f04d5577af42e8b170bb85695a8b212912afaa2
Mickael R <94828882+Micka0675@users.noreply.github.com>
2023-01-30 19:34:26 +0100

    add searchbar v1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-var startResearch = document.getElementById('startResearch');
+// var startResearch = document.getElementById('startResearch');
 var def = document.getElementsByClassName('definition');
 var defBloc = document.getElementsByClassName('tabline');
-
+var main = document.getElementById('main');
+var succes = false;
 var definition = [];
-console.log(defBloc);
-startResearch.addEventListener('click', research);
-
+// startResearch.addEventListener('click', research);
+document.addEventListener('click',checkElement);
+function checkElement(e){
+    if(e.target && e.target.id == "startResearch" && document.getElementById('searchBar').value != undefined && document.getElementById('searchBar').value != "")
+    {
+        console.log(document.getElementById('searchBar').value);
+        research();
+    }
+    if(e.target && e.target.id == "ok")
+    {
+        resetDefs();
+    }
+    if(e.target && e.target.id == "searchBar")
+    {
+        if(e.target.value != "")
+        {
+            e.target.value = "";
+        }
+    }
+}
 
 function research(){
     var searchBarValue = document.getElementById('searchBar').value.toUpperCase();
-    console.log(searchBarValue);
+    document.getElementById('searchBar').value = "";
     for (i=0;i<def.length;i++)
     {
         definition.push(document.getElementsByClassName('definition')[i].innerHTML.toUpperCase());
@@ -17,8 +35,9 @@ function research(){
         // console.log(searchBarValue,definition[i],compare);
         if(compare == 0)
         {
-
-            console.log("similar!");
+            succes = true;
+            main.innerHTML += '<div class="boxValid" id="boxValid"><input class="valid-def" type="button" value="OK" id="ok"></div>';
+            document.getElementById('boxSearch').style.visibility = "hidden";
         }
         else
         {
@@ -26,5 +45,22 @@ function research(){
            defBloc[i].style.display = 'none';
         }
         
+        
+    }
+    if(succes === false)
+    {
+        main.innerHTML += '<div class="boxValid" id="boxValid"><p>Désolé, la recherche n\'a retourné aucun resultat. Veuillez verifier que le mot clé est bien renseigné.</p><input class="valid-def" type="button" value="OK" id="ok"></div>';
+        document.getElementById('boxSearch').style.visibility = "hidden";
+    }
+};
+
+function resetDefs(){
+    for (i=0;i<def.length;i++)
+    {
+        var boxValid = document.getElementById('boxValid');
+        boxValid.style.display ='none';
+        defBloc[i].style.display = 'flex';
+        succes = false;
+        document.getElementById('boxSearch').style.visibility = "visible";
     }
 };
