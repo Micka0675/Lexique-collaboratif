615644c2952eba68a3ca844720d32140e57aafad
Mickael R <94828882+Micka0675@users.noreply.github.com>
2023-02-01 15:02:51 +0100

    add suggestions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// var startResearch = document.getElementById('startResearch');
+// declaration des variables
 var enter = false;
 var def = document.getElementsByClassName('definition');
 var defBloc = document.getElementsByClassName('tabline');
@@ -6,10 +6,13 @@ var main = document.getElementById('main');
 var succes = false;
 var definition = [];
 var dynamElem;
-// startResearch.addEventListener('click', research);
 
+// evenements au clic et a la pression d'une touche sur tout le document
 document.addEventListener('click',checkElement);
 document.addEventListener('keypress',checkElement);
+
+// fonction qui determine quelle zone a été cliquée ou a quelle etape la touche 'entree' a été pressée 
+// avec redirection en consequence
 function checkElement(e){
     if(enter == true)
     {
@@ -19,6 +22,7 @@ function checkElement(e){
     if(e.target && e.target.id == "startResearch" && document.getElementById('searchBar').value != undefined && document.getElementById('searchBar').value != "" || e.key == 'Enter' && document.getElementById('searchBar').value != undefined && document.getElementById('searchBar').value != "")
     {
         research();
+        // reset de la valeur de la barre de recherche
         document.getElementById('searchBar').value = "";
         enter = true;
     }
@@ -26,26 +30,21 @@ function checkElement(e){
     {
         resetDefs();
     }
-
-    // Pour vider la barre de recherche : (utile ?)
-
-    // if(e.target && e.target.id == "searchBar")
-    // {
-    //     if(e.target.value != "")
-    //     {
-    //         e.target.value = "";
-    //     }
-    // }
 }
 
+// comparaison des données de recherche
 function research(){
+    // recuperation de la valeur de la barre de recherche et transformation en majuscules
     var searchBarValue = document.getElementById('searchBar').value.toUpperCase();
+    // reset de la valeur de la barre de recherche
     document.getElementById('searchBar').value = "";
+    // pour tous les elements de classe 'definition'
     for (i=0;i<def.length;i++)
     {
+        // on ajoute l'element dans un tableau
         definition.push(document.getElementsByClassName('definition')[i].innerHTML.toUpperCase());
+        // comparaison des valeurs avec startWith() qui renvoie un booleen
         var compare = definition[i].startsWith(searchBarValue);
-        // console.log(searchBarValue,definition[i],compare);
         if(compare === true)
         {
             succes = true;
@@ -59,6 +58,7 @@ function research(){
     }
     if(succes === false)
     {
+        // creation d'elements qui constituent une div d'information qui renvoient l'info qu'il n'y a aucun resultats avec un bouton valider
         dynamElem = document.createElement("div");
         dynamElem.id = 'boxValid';
         dynamElem.setAttribute("class","boxValid");
@@ -72,11 +72,11 @@ function research(){
         dynamP = document.createElement('p');
         dynamElem.appendChild(dynamP);
         dynamP.innerText = "Désolé, la recherche n'a retourné aucun resultat. Veuillez verifier que le mot clé est bien renseigné.";
-        // main.innerHTML += '<div class="boxValid" id="boxValid"><p></p><input class="valid-def" type="button" value="OK" id="ok"></div>';
         document.getElementById('boxSearch').style.visibility = "hidden";
     }
     else
     {
+        // creation d'elements qui constituent une div d'information avec un bouton valider
         dynamElem = document.createElement("div");
         dynamElem.id = 'boxValid';
         dynamElem.setAttribute("class","boxValid");
@@ -91,6 +91,7 @@ function research(){
     }
 };
 
+// fonction de reset des definitions
 function resetDefs(){
     dynamElem = document.getElementById('boxValid');
     dynamElem.remove();
